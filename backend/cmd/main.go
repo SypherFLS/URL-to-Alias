275a94b7467a139b03a905a1fd63123077c591a1
SypherFLS <www.krasavinlv@gmail.com>
2025-06-29 21:03:18 +0300

    arch

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
-    "fmt"    
+    "log/slog"
+    // "fmt"  
+    "os"  
 	"project/internal/config"
 )
-func main() {    
+
+const (
+    envLocal = "local"
+    envDev = "dev"
+    envProd = "prod"
+)
+
+func main() {   
+
 	cfg := config.MustLoad()
-    fmt.Println(cfg)
+
+    log := setupLogger(cfg.Env)
+
+    log.Info("starting backend", slog.String("env", cfg.Env))
     // TODO: init logger : slog
     // TODO: init db : gorm
     // TODO: init router : chi, chirender
     // TODO: init storage : sqlIte
+}
+
+
+func setupLogger (env string) *slog.Logger  {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+    
+    return log
+
 }
