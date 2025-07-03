12db95cd6bdb7d1b9d5cde08df72cded2bbe0092
SypherFLS <www.krasavinlv@gmail.com>
2025-07-03 17:09:12 +0300

    methods initializeted

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,63 +1,67 @@
 package main
 
 import (
-    "log/slog"
-    // "fmt"  
-    "os"  
-    "project/internal/storage/sqlite"
+	"log/slog"
+	// "fmt"
+	"os"
 	"project/internal/config"
-    "project/internal/lib/logger/sl"
+	"project/internal/lib/logger/sl"
+	"project/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-chi/chi/v5/middleware"
 )
 
 const (
-    envLocal = "local"
-    envDev = "dev"
-    envProd = "prod"
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
-func main() {   
+func main() {
 
 	cfg := config.MustLoad()
 
-    log := setupLogger(cfg.Env)
+	log := setupLogger(cfg.Env)
 
-    log.Info("starting backend", slog.String("env", cfg.Env))
+	log.Info("starting backend", slog.String("env", cfg.Env))
 
-    storage, err := sqlite.New(cfg.StoragePath)
+	storage, err := sqlite.New(cfg.StoragePath)
 
-    if err != nil {
-        log.Error("failed to init db", sl.Err(err))
-        os.Exit(1)
-    }
+	if err != nil {
+		log.Error("failed to init db", sl.Err(err))
+		os.Exit(1)
+	}
 
-    _ = storage
+	_ = storage
 
-    // TODO: init logger : slog
-    // TODO: init db : gorm
-    // TODO: init router : chi, chirender
-    // TODO: init storage : sqlIte
-    
+	router := chi.NewRouter()
+
+	
+	// TODO: init logger : slog
+	// TODO: init db : gorm
+	// TODO: init router : chi, chirender
+	// TODO: init storage : sqlIte
+
 }
 
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
 
-func setupLogger (env string) *slog.Logger  {
-    var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
 
-    switch env {
-    case envLocal:
-        log = slog.New(
-            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-        )
-    case envDev:
-        log = slog.New(
-            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-        )
-    case envProd:
-        log = slog.New(
-            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-        )
-    }
-    
-    return log
+	return log
 
 }
--- backend/internal/storage/sqlite/sqlite.go
+++ backend/internal/storage/sqlite/sqlite.go
@@ -36,3 +36,62 @@ func New(storagePath string) (*Storage, error) {
 
 	return &Storage{db: db}, nil
 }
+
+func (s *Storage) SaveUrl(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveUrl"
+
+	stmt, err := s.db.Prepare(`INSERT INTO url(url, alias) VALUES(?, ?)`)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+
+func (s *Storage) GetUrl(alias string) (string, error) {
+	const op = "storage.sqlite.GetUrl"
+
+	stmt, err := s.db.Prepare(`SELECT url FROM url WHERE alias = ?`)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var resUrl string
+
+	err = stmt.QueryRow(alias).Scan(&resUrl)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return resUrl, nil
+}
+
+func (s *Storage) DeleteUrl(alias string) error {
+	const op = "storage.sqlite.DeleteUrl"
+
+	stmt, err := s.db.Prepare(`DELETE FROM url WHERE alias = ?`)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec(alias)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
